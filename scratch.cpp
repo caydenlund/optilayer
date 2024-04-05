5e9440ba1c105ac44ce704aaa106e0201325ed8f
Cayden Lund <cayden.lund@utah.edu>
2024-04-05 14:37:07 -0600

    Modify `scratch` argument syntax

--- scratch.cpp
+++ scratch.cpp
@@ -3,23 +3,26 @@
 #include <iostream>
 
 int main(const int argc, const char** argv) {
-    for (int ind = 1; ind < argc; ++ind) {
-        std::cout << "Processing file `" << argv[ind] << ":`\n";
+    const std::string inputFile = argv[1];
+    const std::string outputFile = argv[2];
 
-        const Stl stl(std::string {argv[ind]});
+    std::cout << "Processing file `" << inputFile << ":`\n";
 
-        for (const auto& [normals, vertices] : stl.facets) {
-            std::cout << "    Facet:\n"
-                      << "        - Normals:  " << normals[0] << "  " << normals[1] << "  " << normals[2] << "\n"
-                      << "        - Vertices:\n"
-                      << "            - " << vertices[0].coordinates[0] << "  " << vertices[0].coordinates[1] << "  "
-                      << vertices[0].coordinates[2] << "\n"
-                      << "            - " << vertices[1].coordinates[0] << "  " << vertices[1].coordinates[1] << "  "
-                      << vertices[1].coordinates[2] << "\n"
-                      << "            - " << vertices[2].coordinates[0] << "  " << vertices[2].coordinates[1] << "  "
-                      << vertices[2].coordinates[2] << "\n";
-        }
+    const Stl stl(std::string {inputFile});
 
-        stl.saveFile("test.stl");
+    for (const auto& [normals, vertices] : stl.facets) {
+        std::cout << "    Facet:\n"
+                  << "        - Normals:  " << normals[0] << "  " << normals[1] << "  " << normals[2] << "\n"
+                  << "        - Vertices:\n"
+                  << "            - " << vertices[0].coordinates[0] << "  " << vertices[0].coordinates[1] << "  "
+                  << vertices[0].coordinates[2] << "\n"
+                  << "            - " << vertices[1].coordinates[0] << "  " << vertices[1].coordinates[1] << "  "
+                  << vertices[1].coordinates[2] << "\n"
+                  << "            - " << vertices[2].coordinates[0] << "  " << vertices[2].coordinates[1] << "  "
+                  << vertices[2].coordinates[2] << "\n";
     }
+
+    stl.saveFile(outputFile);
+
+    return 0;
 }
